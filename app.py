ac2f89959dc4d9f753cba5b91e5056b37b58c05a
Eslam21 <Es.Ahmed@nu.edu.eg>
2023-06-17 19:27:27 +0300

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ from Eslam_main import GenerateSummary
 
 st.set_page_config(page_title="Study Sync", page_icon="https://github.com/AnasMations/StudySync/blob/36ae1cad78544b8a07239d1cefa141a59f6305c8/img/icon.png?raw=true")
 
+
 def style():
     st.markdown(
         """
